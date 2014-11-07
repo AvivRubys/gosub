1393322b0cd3ef6da91866dbae27203e6e0b916a
Aviv Rozenboim <rubysrubys@gmail.com>
2014-11-07 20:42:58 +0200

    Made things concurrent!

--- providers/providers.go
+++ providers/providers.go
@@ -1,6 +1,9 @@
 package providers
 
-import "log"
+import (
+	"log"
+	"sync"
+)
 
 // SubtitleDB is the interface through which we search for subtitles
 type SubtitleDB interface {
@@ -17,17 +20,35 @@ func (db *providerDB) addSource(s SubtitleProvider) {
 }
 
 func (db *providerDB) SearchAll(fileName, language string) ([]Subtitle, error) {
-	var subs []Subtitle
-	for _, src := range db.providers {
-		srcSubs, err := src.GetSubtitles(fileName, language)
-		if err != nil {
-			log.Printf("ERR When getting subtitles from %s: %s", src.Name(), err)
-		} else {
-			subs = append(subs, srcSubs...)
-		}
+	var result []Subtitle
+	var wg sync.WaitGroup
+	subReceiver := make(chan []Subtitle)
+
+	// Launch a goroutine for each provider, send results into subReceiver channel
+	for _, provider := range db.providers {
+		wg.Add(1)
+		go func(p SubtitleProvider) {
+			defer wg.Done()
+			subs, err := p.GetSubtitles(fileName, language)
+			if err != nil {
+				log.Printf("ERR When getting subtitles from %s: %s", p.Name(), err)
+			} else {
+				subReceiver <- subs
+			}
+		}(provider)
 	}
 
-	return subs, nil
+	// When work is done, close the channel, leaving up the loop below
+	go func() {
+		wg.Wait()
+		close(subReceiver)
+	}()
+
+	for subs := range subReceiver {
+		result = append(result, subs...)
+	}
+
+	return result, nil
 }
 
 var searchers = providerDB{make([]SubtitleProvider, 0)}

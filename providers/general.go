f8890fb24523c38f88d9e68e8bb683a75826ca50
Aviv Rozenboim <rubysrubys@gmail.com>
2014-11-07 15:22:17 +0200

    Refractored providers.go such that: * *providers.go* contains the provider registry * *general.go* contains the basic structs and interfaces of the providers package.

---
+++ providers/general.go
@@ -0,0 +1,25 @@
+package providers
+
+// Basic structs and interfaces of the providers module
+
+// Subtitle is a reference to a subtitle, i.e. it wasn't downloaded yet, just found.
+type Subtitle struct {
+	FileName  string
+	Hash      string
+	Format    string
+	Downloads int
+	URL       string
+	Source    SubtitleProvider
+}
+
+// SubtitleProvider is the interface a provider needs to implement
+type SubtitleProvider interface {
+	// Name returns the name of this provider
+	Name() string
+
+	// GetSubtitle accepts a filepath and a language, and searches for subtitles
+	GetSubtitle(filePath, language string) ([]Subtitle, error)
+
+	// Download returns the path of the downloaded subtitle
+	Download(subtitle Subtitle, filePath string) (string, error)
+}
